7160cb4cbaba368080fef7f021978848a4823833
Elliot Pahl <elliot.pahl@gmail.com>
2021-12-29 23:11:50 +1100

    Fix kyria oled layer list

--- keyboards/splitkb/kyria/keymaps/halcyoncorsair/keymap.c
+++ keyboards/splitkb/kyria/keymaps/halcyoncorsair/keymap.c
@@ -243,7 +243,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * DO NOT edit the rev1.c file; instead override the weakly defined default functions by your own.
  */
 
-/* DELETE THIS LINE TO UNCOMMENT (1/2)
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
@@ -263,20 +262,23 @@ bool oled_task_user(void) {
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
         switch (get_highest_layer(layer_state|default_layer_state)) {
-            case _QWERTY:
-                oled_write_P(PSTR("QWERTY\n"), false);
-                break;
             case _COLEMAK_DH:
                 oled_write_P(PSTR("Colemak-DH\n"), false);
                 break;
-            case _NAV:
-                oled_write_P(PSTR("Nav\n"), false);
+            case _QWERTY:
+                oled_write_P(PSTR("QWERTY\n"), false);
                 break;
             case _SYM:
                 oled_write_P(PSTR("Sym\n"), false);
                 break;
-            case _FUNCTION:
-                oled_write_P(PSTR("Function\n"), false);
+            case _NAV:
+                oled_write_P(PSTR("Nav\n"), false);
+                break;
+            case _NUM:
+                oled_write_P(PSTR("Num\n"), false);
+                break;
+            case _WINMGMT:
+                oled_write_P(PSTR("Divvy\n"), false);
                 break;
             case _ADJUST:
                 oled_write_P(PSTR("Adjust\n"), false);
@@ -308,8 +310,6 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
-
-DELETE THIS LINE TO UNCOMMENT (2/2) */
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
