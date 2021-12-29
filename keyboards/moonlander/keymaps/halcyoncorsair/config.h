5fa34cd62e5f92b3a223b7c98c017a3f82965372
Elliot Pahl <elliot.pahl@gmail.com>
2021-12-29 23:06:25 +1100

    Set tap toggle to 2

--- keyboards/moonlander/keymaps/halcyoncorsair/config.h
+++ keyboards/moonlander/keymaps/halcyoncorsair/config.h
@@ -34,7 +34,7 @@
 // Other settings //
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
-#define TAPPING_TOGGLE 1
+#define TAPPING_TOGGLE 2
 
 // Recommended for heavy chording.
 #define QMK_KEYS_PER_SCAN 4
