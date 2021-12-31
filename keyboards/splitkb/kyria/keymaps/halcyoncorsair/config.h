25a58deddc8ce419cc9981057ff29dafe824feae
Elliot Pahl <elliot.pahl@gmail.com>
2021-12-31 12:00:33 +1100

    Fix encoder settings

--- keyboards/splitkb/kyria/keymaps/halcyoncorsair/config.h
+++ keyboards/splitkb/kyria/keymaps/halcyoncorsair/config.h
@@ -53,3 +53,14 @@
 #    undef TAPPING_TERM
 #endif  // TAPPING_TERM
 #define TAPPING_TERM 200
+
+#define ENCODER_DIRECTION_FLIP
+
+// EC11K encoders have a different resolution than other EC11 encoders.
+// When using the default resolution of 4, if you notice your encoder skipping
+// every other tick, lower the resolution to 2.
+#define ENCODER_RESOLUTION 2
+
+// Allows to use either side as the master. Look at the documentation for info:
+// https://docs.qmk.fm/#/config_options?id=setting-handedness
+#define EE_HANDS
