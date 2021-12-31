4575e4137ff234e722a2dc7158a9dd69986f34cb
Elliot Pahl <elliot.pahl@gmail.com>
2021-12-31 12:38:54 +1100

    Turn off features for firmware size

--- keyboards/splitkb/kyria/keymaps/halcyoncorsair/rules.mk
+++ keyboards/splitkb/kyria/keymaps/halcyoncorsair/rules.mk
@@ -2,3 +2,10 @@ OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+# Need to reduce firmware size
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
