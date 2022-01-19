d99fc4c4a7bf75007be9953629a2a92fad15e58e
Elliot Pahl <elliot.pahl@gmail.com>
2022-01-20 00:12:26 +1100

    Update moonlander layout to kyria current

--- keyboards/moonlander/keymaps/halcyoncorsair/keymap.c
+++ keyboards/moonlander/keymaps/halcyoncorsair/keymap.c
@@ -28,11 +28,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT_moonlander(
         XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    ADJUST,       QWERTY,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX,      XXXXXXX,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX,      XXXXXXX,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
         KC_ESC,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,    KC_HYPR,      KC_MEH,   KC_M,    HOME_N,  HOME_E,  HOME_I,  HOME_O,  KC_QUOT,
-        OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                            KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, OS_RSFT,
+        OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                            KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, NAV,              SPOTLIGHT,    ONEPASS,           SYM,     REPEAT,  XXXXXXX, XXXXXXX, XXXXXXX,
-                                            KC_SPC,  KC_BSPC, DIVVY,        ADJUST,   KC_TAB,  KC_ENT
+                                            KC_SPC,  KC_BSPC, KC_MEH,       ADJUST,   KC_TAB,  KC_ENT
     ),
 
     [_QWERTY] = LAYOUT_moonlander(
@@ -46,48 +46,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYM] = LAYOUT_moonlander(
         VRSN,    _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_EXLM, KC_AT,   KC_SCLN, KC_COLN, KC_UNDS, _______,      _______, KC_EQL,  KC_AMPR, KC_GRAVE,KC_TILD, KC_PLUS, _______,
-        _______, HOME_BSLS,HOME_PIPE,HOME_LCBR,HOME_LPRN,KC_LBRC,_______,   _______, KC_ASTR, HOME_DLR,HOME_PERC,HOME_CIRC,HOME_MINS,_______,
-        _______, KC_TILD, KC_GRAVE,KC_RCBR, KC_RPRN, KC_RBRC,                        KC_AMPR, KC_EXLM, KC_AT,   KC_HASH, _______, _______,
-        _______,   _______, _______, _______, NUM,           _______,       _______,          _______, _______, _______, _______, _______,
-                                            _______, KC_DEL, SH_OS,         _______, _______, _______
+        KC_GRV,  KC_4,    KC_3,    KC_2,    KC_1,    KC_5,    _______,      _______, KC_9,    KC_0,    KC_6,    KC_7,    KC_8,    _______,
+        KC_DEL,  HOME_DLR,HOME_PLUS,HOME_LPRN,HOME_RPRN,KC_AT,_______,      _______, KC_PIPE, HOME_MINS,HOME_EQL,HOME_UNDS,HOME_ASTR,_______,
+        _______, KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_TILD,                        KC_AMPR, KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, _______,
+        _______, _______, _______, _______, SH_OS,            _______,      _______,          _______, _______, _______, _______, _______,
+                                            _______, _______, _______,      _______, _______, _______
     ),
 
     [_NAV] = LAYOUT_moonlander(
         _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,      _______, KC_PGUP, MHOME,   KC_UP,   MEND,     KC_VOLU, KC_DEL,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, _______,      _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_VOLD, KC_CAPS,
-        _______, _______, CUT,     COPY,    PASTE,   PTXT,                           XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT,  KC_MUTE, KC_INS,
-        _______, _______, _______, _______, _______,          _______,      _______,          NUM,     SELWORD, _______,  _______, _______,
-                                            _______, _______, _______,      _______, _______, _______
-    ),
-
-    [_NUM] = LAYOUT_moonlander(
-        _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,      _______, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_PLUS, KC_BSPC,
-        _______, HOME_F5, HOME_F6, HOME_F7, HOME_F8, _______, _______,      _______, KC_0,    HOME_4,  HOME_5,  HOME_6,  HOME_MINS,_______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                        KC_ASTR, KC_1,    KC_2,    KC_3,    KC_SLSH, _______,
-        _______, _______, _______, _______, _______,          _______,      _______,          _______, _______, KC_DOT,  _______, _______,
-                                            _______, _______, _______,      _______, _______, _______
-    ),
-
-    [_WINMGMT] = LAYOUT_moonlander(
-        _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______,      _______, _______, DIVVY_U, DIVVY_I, DIVVY_O, _______, _______,
-        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,      _______, DIVVY_H, DIVVY_J, DIVVY_K, DIVVY_L, DIVVY_SCLN,DIVVY_PANEL,
-        _______, _______, _______, _______, _______, _______,                        _______, DIVVY_M, DIVVY_COMMA,DIVVY_DOT,_______,_______,
-        _______, _______, _______, _______, _______,          _______,      _______,          _______, _______, _______, _______, _______,
-                                            _______, _______, _______,      _______, _______, _______
+        KC_F13,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,      _______, KC_PGUP, MHOME,   KC_UP,   MEND,     KC_VOLU, KC_DEL,
+        KC_F14,  HOME_F5, HOME_F6, HOME_F7, HOME_F8, _______, _______,      _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_VOLD, KC_CAPS,
+        KC_F15,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   PTXT,                           XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT,  KC_MUTE, KC_INS,
+        _______, _______, _______, _______, _______,          _______,      _______,          _______, SELWORD, _______,  _______, _______,
+                                            _______, _______, _______,      _______, KC_BSPC, _______
     ),
 
     // TODO: Redefine the divvy shortcuts above
     [_ADJUST] = LAYOUT_moonlander(
-        WEBUSB_PAIR,_______,_______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, RESET,
-        _______, _______, _______,   QWERTY,  _______, _______, _______,    _______, _______, _______, _______, RGB_TOG, _______, DEBUG,
-        _______, _______, _______,   COLEMAK, _______, _______, _______,    _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, _______,
-        _______, _______, _______,   _______, _______, _______,                      _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______,
-        _______, _______, _______,   _______, _______,          _______,    _______,          _______, _______, _______, _______, _______,
-                                              _______, _______, _______,    _______, KC_BRID, KC_BRIU
+        WEBUSB_PAIR,_______,_______,_______,_______, _______, _______,      _______, _______, _______, _______, _______, _______, RESET,
+        _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, DEBUG,
+        _______, _______, _______, COLEMAK, QWERTY,  _______, _______,      _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, _______,
+        _______, _______, _______, _______, _______, _______,                        _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______,
+        _______, _______, _______, _______, _______,          _______,      _______,          _______, _______, _______, _______, _______,
+                                            _______, _______, _______,      _______, KC_BRID, KC_BRIU
     ),
 };
 
