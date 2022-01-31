ed29712a31fecb838e7f5a9c66e8ff20e49a556c
Elliot Pahl <elliot.pahl@gmail.com>
2022-01-31 14:57:32 +1100

    Tweak config and whitespace

--- keyboards/splitkb/kyria/keymaps/halcyoncorsair/rules.mk
+++ keyboards/splitkb/kyria/keymaps/halcyoncorsair/rules.mk
@@ -1,14 +1,14 @@
-OLED_ENABLE 		= yes
-OLED_DRIVER 		= SSD1306   	# Enables the use of OLED displays
-ENCODER_ENABLE  	= yes   # 1018 (with my config)	# Enables the use of one or more encoders
+OLED_ENABLE			= yes
+OLED_DRIVER			= SSD1306   	# Enables the use of OLED displays
+ENCODER_ENABLE		= yes   	# 1018 (with my config)	# Enables the use of one or more encoders
 
-RGBLIGHT_ENABLE 	= no    # 2108  # Enable keyboard RGB underglow
-RGB_MATRIX_ENABLE 	= yes	# 2520
+RGBLIGHT_ENABLE		= no    # 2108  # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE	= yes	# 2520
 
-COMBO_ENABLE 	  	= yes
-# LEADER_ENABLE		= no	# 162
-NKRO_ENABLE 	  	= yes	# 316
-# SWAP_HANDS_ENABLE   = no    # 570
+COMBO_ENABLE		= yes
+LEADER_ENABLE		= no	# 162
+NKRO_ENABLE			= yes	# 316
+SWAP_HANDS_ENABLE	= no    # 570
 
 # Userspace features
 CAPS_WORD_ENABLE	= yes
@@ -16,12 +16,12 @@ REPEAT_KEY_ENABLE	= yes	# 276
 SELECT_WORD_ENABLE	= yes
 
 # Need to reduce firmware size
-LTO_ENABLE 			= yes
-CONSOLE_ENABLE 		= no
-COMMAND_ENABLE 		= no
-MOUSEKEY_ENABLE 	= no
+LTO_ENABLE			= yes
+CONSOLE_ENABLE		= no
+COMMAND_ENABLE		= no
+MOUSEKEY_ENABLE		= no
 # EXTRAKEY_ENABLE = no // this includes media keys and system volue control
-SPACE_CADET_ENABLE 	= no
-GRAVE_ESC_ENABLE 	= no	# 82
-MAGIC_ENABLE 		= no
-MUSIC_ENABLE 		= no
+SPACE_CADET_ENABLE	= no
+GRAVE_ESC_ENABLE	= no	# 82
+MAGIC_ENABLE		= no
+MUSIC_ENABLE		= no
