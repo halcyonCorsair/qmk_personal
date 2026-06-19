e98954d3152a27cef4c8d7c977cf082081743919
Elliot Pahl <elliot.pahl@gmail.com>
2026-06-19 20:49:54 +1000

    update skeletyl

--- keyboards/bastardkb/skeletyl/keymaps/halcyoncorsair/config.h
+++ keyboards/bastardkb/skeletyl/keymaps/halcyoncorsair/config.h
@@ -18,11 +18,6 @@
 
 #define TAPPING_TERM_PER_KEY // See bottom of keymap.c
 
-// // Lets you roll mod-tap keys
-// #define IGNORE_MOD_TAP_INTERRUPT
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 // #define TAPPING_FORCE_HOLD
 
@@ -33,6 +28,7 @@
 #undef PERMISSIVE_HOLD
 
 // Other settings //
+#define HOLD_ON_OTHER_KEY_PRESS
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
 #define TAPPING_TOGGLE 2
@@ -79,3 +75,5 @@
 // #   undef RGB_DISABLE_WHEN_USB_SUSPENDED
 // #   define RGB_DISABLE_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
 #endif
+
+#define SELECT_WORD_OS_MAC
